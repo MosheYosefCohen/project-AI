e436573d97ae8bdf2c0046ffc4359bfc2fde8240
natipo1221 <132401812+natipo1221@users.noreply.github.com>
2023-05-14 16:28:26 +0300

    Update classss.py
    create new customers and products

--- classss.py
+++ classss.py
@@ -1,35 +1,50 @@
-import random
 class Customer:
-    def _init_(self, customer_id, name, email, phone):
+    def __init__(self, customer_id, name, email, phone):
         self.customer_id = customer_id
         self.name = name
         self.email = email
         self.phone = phone
 
+    def __str__(self):
+        return f"Name: {self.name}, Email: {self.email}, customer ID: {self.customer_id}, Phone: {self.phone}"
+
 
 class Product:
-    def _init_(self, product_id, name, price):
+    def __init__(self, product_id, name, price):
         self.product_id = product_id
         self.name = name
         self.price = price
 
+    def __str__(self):
+        return f"Name: {self.name}, Price: {self.price}, product ID: {self.product_id}"
+
 
 class OrderItem:
-    def _init_(self, product, quantity):
+    def __init__(self, product, quantity):
         self.product = product
         self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.product}, Quantity: {self.quantity}"
 
     def get_total_price(self):
         return self.product.price * self.quantity
 
 
 class Order:
-    def _init_(self, order_id, customer):
+    def __init__(self, order_id, customer):
         self.order_id = order_id
         self.customer = customer
         self.items = []
 
+    def __str__(self):
+        return f"order id: {self.order_id}, {self.customer}"
+
     def add_item(self, product, quantity):
+        for i in self.items:
+            if i.product == product:
+                i.quantity += quantity
+                return
         order_item = OrderItem(product, quantity)
         self.items.append(order_item)
 
@@ -41,7 +56,7 @@ class Order:
 
 
 class OrderManagementSystem:
-    def _init_(self):
+    def __init__(self):
         self.customers = {}
         self.products = {}
         self.orders = {}
@@ -77,8 +92,22 @@ class OrderManagementSystem:
         order = self.orders[order_id]
         return order.get_total_price()
 
+
+store = OrderManagementSystem()
 lst_names = ["Emma", "Noah", "Olivia", "Liam", "Ava", "William", "Sophia", "Mason", "Isabella", "James", "Mia", "Benjamin", "Charlotte", "Jacob", "Amelia", "Michael", "Harper", "Elijah", "Evelyn", "Ethan", "Abigail", "Alexander", "Emily", "Daniel", "Elizabeth", "Matthew", "Mila", "Aiden", "Ella", "Henry"]
 lst_products = ['T-shirt', 'Jeans', 'Sneakers', 'Watch', 'Sunglasses', 'Perfume', 'Handbag', 'Backpack', 'Jacket', 'Hoodie', 'Sweater', 'Skirt', 'Dress', 'High heels', 'Running shoes', 'Smartphone', 'Laptop', 'Tablet', 'Headphones', 'Bluetooth speaker', 'Gaming console', 'Camera', 'Fitness tracker', 'Smartwatch', 'Virtual reality headset', 'Drone', 'Wireless earbuds', 'Fitness equipment', 'Electric razor', 'Air purifier']
 lst_phone_numbers = ["1234567890", "9876543210", "5555555555", "1111111111", "9999999999", "4444444444", "8888888888", "6666666666", "7777777777", "2222222222", "3333333333", "5551234567", "8885559999", "1112223333", "4445556666", "7778889999", "1231231234", "4564564567", "7897897890", "3213213210", "6546546540", "8881234567", "5555551212", "9879879870", "3334445555", "6667778888", "1112223334", "4445556667", "7778889990", "1231231235", "4564564568"]
 lst_email = ["emma@example.com", "noah@example.com", "olivia@example.com", "liam@example.com", "ava@example.com", "william@example.com", "sophia@example.com", "mason@example.com", "isabella@example.com", "james@example.com", "mia@example.com", "benjamin@example.com", "charlotte@example.com", "jacob@example.com", "amelia@example.com", "michael@example.com", "harper@example.com", "elijah@example.com", "evelyn@example.com", "ethan@example.com", "abigail@example.com", "alexander@example.com", "emily@example.com", "daniel@example.com", "elizabeth@example.com", "matthew@example.com", "mila@example.com", "aiden@example.com", "ella@example.com", "henry@example.com"]
 lst_price = [20.0, 50.0, 80.0, 200.0, 100.0, 60.0, 150.0, 70.0, 120.0, 50.0, 80.0, 40.0, 100.0, 90.0, 100.0, 800.0, 1200.0, 500.0, 150.0, 100.0, 400.0, 600.0, 80.0, 300.0, 500.0, 1000.0, 120.0, 500.0, 50.0, 300.0]
+for i in range(30):
+    store.add_customer(i, lst_names[i], lst_email[i], lst_phone_numbers[i])
+    store.add_product(i, lst_products[i], lst_price[i])
+
+# for i in range(30):
+#     print(store.customers[i])
+#
+# for i in range(30):
+#     print(store.products[i])
+
+
+
